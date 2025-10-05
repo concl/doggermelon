3ab1eebe27b51791156df9a49f353e3fa6488c65
concl <107962474+concl@users.noreply.github.com>
2025-10-04 22:36:19 -0700

    main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,8 +1,28 @@
 extends Node3D
-
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var start_button: Button = $MenuUI/PanelContainer/StartButton
 const MAIN = preload("res://scenes/main.tscn")
 
 
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+    if anim_name == "fade_out":
+        get_tree().change_scene_to_packed(MAIN)
+
+# UI Logic
+@onready var main: Control = $MenuUI/Main
+@onready var options: Control = $MenuUI/Options
+
+
 func _on_start_button_pressed() -> void:
-    get_tree().change_scene_to_packed(MAIN)
+    animation_player.play("fade_out")
+
+
+func _on_options_button_pressed() -> void:
+    main.hide()
+    options.show()
+
+
+func _on_back_button_pressed() -> void:
+    main.show()
+    options.hide()
