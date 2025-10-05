0794bc1af8e8b3b5abb1e321476674c743ba4c1f
Amy Sun <mymngtnsn@gmail.com>
2025-10-04 23:59:18 -0700

    clearing

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,7 +3,7 @@ extends Node2D
 const BALL_CLASS = preload("res://scenes/balls.tscn")
 
 var current_ball: RigidBody2D = null
-var spawn_level = 0
+var gamestage = 0
 
 func _ready():
     spawn_new_ball()
@@ -13,7 +13,7 @@ func spawn_new_ball():
         return
 
     current_ball = BALL_CLASS.instantiate()
-    current_ball.spawn(spawn_level)
+    current_ball.spawn(gamestage)
     add_child(current_ball)
 
     var screen_size = get_viewport().size
@@ -27,7 +27,13 @@ func _process(_delta):
     if current_ball:
         var mouse_x = get_viewport().get_mouse_position().x
         current_ball.global_position.x = clamp(mouse_x, 50, get_viewport().size.x - 50)
-    $Score.text = "Score: " + str(calculate_score())
+    
+    var score = calculate_score()
+    $Score.text = "Score: " + str(score)
+    if score > 50 && score < 200:
+        gamestage = 1
+    if score > 200:
+        gamestage = 0
 
 func _input(event):
     if event.is_action_pressed("drop_ball") and current_ball and current_ball.can_drop == true:
@@ -35,9 +41,19 @@ func _input(event):
         current_ball = null
         spawn_new_ball()
 
+func level_to_value(level) -> int:
+    return 2**(level+1)
+
 func calculate_score() -> int:
     var score = 0
     for node in get_tree().current_scene.get_children():
         if node is RigidBody2D:
-            score += 2**(node.lvl + 1)
+            score += level_to_value(node.lvl)
     return score
+
+func clear_all_balls():
+    var total_score = 0
+    for node in get_tree().current_scene.get_children():
+        if node.is_class("RigidBody2D"):
+            var xp = level_to_value(node.lvl)
+            node.queue_free()
