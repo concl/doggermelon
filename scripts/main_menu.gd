a52ccb2660f1d51d170b67484a545cd0cf9a09bb
concl <bonumconcl@gmail.com>
2025-10-06 14:48:15 -0700

    everything

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -33,3 +33,10 @@ func _on_back_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_check_box_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
