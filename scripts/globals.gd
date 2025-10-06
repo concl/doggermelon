6607c68a7c5426c8e0ba80ad08188afdd5ecb874
KevinYang <yangzijin@gmail.com>
2025-10-05 17:20:37 -0700

    crude ceiling implementation

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -4,15 +4,17 @@ enum GameStage { EARLY, MID, LATE }
 
 signal xp_changed
 
+signal clear_all_balls
+
 var xp = 0
 var level = 1
 
 var collectibles = []
 
 func update_xp(added):
-    xp += added
-    xp_changed.emit()
+	xp += added
+	xp_changed.emit()
 
 
 func add_collectible(collectible):
-    collectibles.append(collectible)
+	collectibles.append(collectible)
